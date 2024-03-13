25693474d119bda5621b565b934205f7107c3721
msingh <msingh@cogitate.us>
2024-03-13 10:23:20 +0530

    ref# 031324 - add ejs view engine setup

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const PORT = 8000;
 // use express router
 app.use('/', require('./routes'));
 
+// setting up view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.listen(PORT, (error) => {
   if(error) console.error(`Server faced an error: ${JSON.stringify(error)}`);
   console.log(`Server is running on port: ${PORT}`);
